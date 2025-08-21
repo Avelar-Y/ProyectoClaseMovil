8162ef7af32a155d3fc64917fc5b51364ee7eec6
Avelar-Y <avelaryesser@gmail.com>
2025-08-20 20:22:27 -0600

    Agregando componente CustomImput

--- Movil/src/screens/Login.tsx
+++ Movil/src/screens/Login.tsx
@@ -1,11 +1,36 @@
-import { View } from 'react-native';
 import CustomButton from '../components/CustomButton';
+import CustomInput from '../components/CustomInput';
+import { View } from 'react-native';
+import React, { useState } from 'react';
 
 export default function Login() {
+  const [email, setemail] = useState('');
   return (
     <View>
       <CustomButton title="Iniciar Sesion" onPress={() => {}} />
-      <CustomButton title="Registrarme" onPress={() => {}} />
+      <CustomInput
+        title="Hola"
+        value={email}
+        type="email"
+        onChange={setemail}
+      />
+
+      <CustomButton
+        title="Registrarme"
+        onPress={() => {}}
+        variant={'secondary'}
+      />
+      <CustomInput
+        title="Hola"
+        value="registrar"
+        type="email"
+        onChange={() => {}}
+      />
+      <CustomButton
+        title="Cambiar contraseña"
+        onPress={() => {}}
+        variant={'tertiary'}
+      />
     </View>
   );
 }
